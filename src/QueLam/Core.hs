f2e88608f9ea15327ea05b4e1d3dc245a53de173
Ilya Yanok <ilya.yanok@gmail.com>
2018-06-24 17:05:12 +0200

    core: add table, needs AllowAmbiguousTypes

--- src/QueLam/Core.hs
+++ src/QueLam/Core.hs
@@ -1,10 +1,18 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE TypeFamilies          #-}
 module QueLam.Core where
+
+import           GHC.TypeLits
 
 -- Implementation of Suzuki, Kiselyov and Kameyama paper
 
 infix 5 =%
 infixl 2 @%
+
+class HasTable db (t :: Symbol) r
 
 class Symantics repr where
   type Obs repr :: * -> *
@@ -19,4 +27,5 @@ class Symantics repr where
   nil    :: repr [a]
   (@%)   :: repr [a] -> repr [a] -> repr [a]
   (=%)   :: repr a -> repr a -> repr Bool
+  table  :: forall db t r. HasTable db t r => repr [r]
   observe :: repr a -> Obs repr a
--- src/QueLam/Example/Q1.hs
+++ src/QueLam/Example/Q1.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module QueLam.Example.Q1 where
 
 import           QueLam.Core
 
 type Order = Int -- just for testing, should be Record
 
-table_orders :: Symantics repr => repr [Order]
-table_orders = undefined
+table_orders :: forall repr db. (Symantics repr, HasTable db "orders" Order) => repr [Order]
+table_orders = table @repr @db @"orders"
 
-q1 :: Symantics repr => repr (Int -> [Order])
+q1 :: forall repr db. (Symantics repr, HasTable db "orders" Order) => repr (Int -> [Order])
 q1 = lam $ \xoid ->
-  for table_orders $ \o ->
+  for (table @repr @db @"orders") $ \o ->
     where' (o =% xoid) $
       yield o
