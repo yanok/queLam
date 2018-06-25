76f5be3542864d322f8761b4c3f2226a2b059e17
Ilya Yanok <ilya.yanok@gmail.com>
2018-06-25 16:03:10 +0200

    ex/q2: not working Q2 from the paper

---
+++ src/QueLam/Example/Q2.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLabels      #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+module QueLam.Example.Q2 where
+
+import           Data.Proxy
+import           QueLam.Core
+import           SuperRecord
+
+q2 ::
+  forall repr schema prod order.
+  ( Symantics repr
+  , HasTable schema "products" (Record prod)
+  , Has "oid" order Int
+  , Has "pid" prod Int)
+  => repr schema (Rec order -> [Record '["pid" := Int]])
+q2 = lam $ \o ->
+  for (table $ Proxy @ "products") $ \p ->
+    where' (p .% #pid =% o .% #oid) $
+      yield ((#pid := (p .% #pid)) &% rnil')
