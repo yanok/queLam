c0cdcd06a8c26b05074787740ec9046f852152aa
Ilya Yanok <ilya.yanok@gmail.com>
2018-06-26 10:58:43 +0200

    ex/q3: test compose

---
+++ src/QueLam/Example/Q3.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLabels      #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+module QueLam.Example.Q3 where
+
+import           Data.Proxy
+import           QueLam.Core
+import           SuperRecord
+
+import QueLam.Example.Q1
+import QueLam.Example.Q2
+
+q3 ::
+  forall repr schema prod order.
+  ( Symantics repr
+  , HasTable schema "orders" order
+  , HasTable schema "products" prod
+  , Has "oid" (Sort order) Int
+  , Has "qty" (Sort order) Int
+  , Has "pid" (Sort prod) Int
+  , Has "name" (Sort prod) String
+  , Has "price" (Sort prod) Int)
+  => repr schema (Int -> [Record '["pid" := Int, "name" := String, "sale" := Int]])
+q3 = compose $$ q1 $$ q2
