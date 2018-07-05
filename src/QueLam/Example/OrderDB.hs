c4540e7981afa7795ccf67bbccd999325cb5f47a
Ilya Yanok <ilya.yanok@gmail.com>
2018-07-05 14:44:00 +0200

    example: update OrderDB to contain the tables from the paper

--- src/QueLam/Example/OrderDB.hs
+++ src/QueLam/Example/OrderDB.hs
@@ -9,11 +9,25 @@ import           SuperRecord
 import           QueLam.Core
 import           QueLam.R
 
---type OrderDBSchema = '[ '("orders", '[ "oid" := Int])]
+type ProductTable = '[ "name" := String, "pid" := Int, "price" := Int] -- has to be sorted
+type OrderTable = '[ "oid" := Int, "pid" := Int, "qty" := Int]
+type OrderDBSchema = '[ '("orders", OrderTable), '("products", ProductTable)]
 
-orderDB :: Handle R '[ '("orders", '[ "oid" := Int])]
+orderDB :: Handle R OrderDBSchema
 orderDB = #orders :=
-  [ #oid := 0 & rnil
-  , #oid := 1 & rnil
-  ]
-  & rnil
+  [ #oid := 1 & #pid := 1 & #qty := 5 & rnil
+  , #oid := 1 & #pid := 2 & #qty := 5 & rnil
+  , #oid := 1 & #pid := 4 & #qty := 2 & rnil
+  , #oid := 1 & #pid := 5 & #qty := 10 & rnil
+  --, #oid := 2 & #pid := 6 & #qty := 20 & rnil
+  , #oid := 3 & #pid := 2 & #qty := 50 & rnil
+  ] &
+  #products :=
+  [ #pid := 1 & #name := "Tablet" & #price := 500 & rnil
+  , #pid := 2 & #name := "Laptop" & #price := 1000 & rnil
+  , #pid := 3 & #name := "Desktop" & #price := 1000 & rnil
+  , #pid := 4 & #name := "Router" & #price := 150 & rnil
+  , #pid := 5 & #name := "HDD" & #price := 100 & rnil
+  , #pid := 6 & #name := "SSD" & #price := 500 & rnil
+  ] &
+  rnil
--- src/QueLam/Example/TestQ1.hs
+++ src/QueLam/Example/TestQ1.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds            #-}
 {-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE OverloadedLabels #-}
 module QueLam.Example.TestQ1 where
 
 import           GHC.TypeLits
@@ -10,5 +11,11 @@ import           QueLam.Example.OrderDB
 import           QueLam.Example.Q1
 import           QueLam.R
 
-test :: R '[ '("orders", '[ "oid" := Int])] (Int -> [Record '["oid" := Int]])
-test = q1 orderDB
+testQ1 :: R OrderDBSchema (Int -> [Record OrderTable])
+testQ1 = q1 orderDB
+
+testO :: R OrderDBSchema ([Record OrderTable])
+testO = table orderDB #orders
+
+testP :: R OrderDBSchema ([Record ProductTable])
+testP = table orderDB #products
