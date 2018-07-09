82d3b6255f650556e71b4fa77e070a7e74bcc653
Ilya Yanok <ilya.yanok@gmail.com>
2018-07-09 11:35:02 +0200

    opts: add ForFor optimization

---
+++ src/QueLam/Optimize/ForFor.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module QueLam.Optimize.ForFor where
+
+import           GHC.TypeLits
+
+import           QueLam.Core
+
+
+data ForFor repr (schema :: [(Symbol, [*])]) a where
+  For :: ForFor repr schema [a]
+      -> (ForFor repr schema a -> ForFor repr schema [b])
+      -> ForFor repr schema [b]
+  Unknown :: repr schema a -> ForFor repr schema a
+
+forFor :: Symantics repr => ForFor repr schema a -> repr schema a
+forFor (For xs f)  = for (forFor xs) $ \x -> forFor $ f $ Unknown x
+forFor (Unknown x) = x
+
+
+instance Symantics repr => RR ForFor repr where
+  fwd = Unknown
+  bwd = forFor
+
+instance Symantics repr => Symantics (ForFor repr) where
+  for xs@(Unknown xs') f = For xs f
+  for (For ys g) f = fwd $ for (bwd ys) $ \y ->
+                             for (bwd $ g $ fwd y) $ \x -> bwd $ f $ fwd x
