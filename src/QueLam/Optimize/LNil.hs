a1d20b07aa6a2dd5dfe6c4321660ace789b86020
Ilya Yanok <ilya.yanok@gmail.com>
2018-07-09 11:23:15 +0200

    LNil: rename dyn to lnil

--- src/QueLam/Optimize/LNil.hs
+++ src/QueLam/Optimize/LNil.hs
@@ -14,16 +14,16 @@ data LNil repr (schema :: [(Symbol, [*])]) a where
   Empty :: LNil repr schema [a]
   Unknown :: repr schema a -> LNil repr schema a
 
-dyn :: Symantics repr => LNil repr schema a -> repr schema a
-dyn Empty       = nil
-dyn (Unknown x) = x
+lnil :: Symantics repr => LNil repr schema a -> repr schema a
+lnil Empty       = nil
+lnil (Unknown x) = x
 
 
 instance Symantics repr => RR LNil repr where
   fwd = Unknown
-  bwd = dyn
+  bwd = lnil
 
 instance Symantics repr => Symantics (LNil repr) where
   nil = Empty
   Empty @% y = y
-  (Unknown x) @% y = Unknown $ x @% dyn y
+  (Unknown x) @% y = Unknown $ x @% lnil y
